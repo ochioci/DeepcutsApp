2e01125e9a1d94f0c94aa90f5b5868b886d13132
ochioci <63116595+ochioci@users.noreply.github.com>
2025-03-25 02:30:47 -0400

    PDF GENERATION IS WORKING!

--- app/getreport/_layout.tsx
+++ app/getreport/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack, Tabs } from "expo-router";
+
 export default function RootLayout() {
     return <Stack>
         <Stack.Screen name={"index"} options={{headerShown: false}}></Stack.Screen>
